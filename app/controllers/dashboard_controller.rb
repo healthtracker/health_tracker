1e8b49f27381975126b37de47ec476ec3b5b4c02
TurnerMooreDavis <turnermdavis@gmail.com>
2015-06-04 16:53:29 -0400

    add calculate calories method to Step class and added calories instance variable to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @calories = (ConsumedCal.daily_calories_consumed - Step.calculate_calories - Exercise.daily_calories_burned)
   end
 end
--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class StepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can return calories burned today" do
+    assert_equal 175, Step.calculate_calories
+    assert_difference "Step.calculate_calories", 2250 do
+      Step.create(steps: 45000, stepped_on: Time.now)
+    end
+  end
 end
